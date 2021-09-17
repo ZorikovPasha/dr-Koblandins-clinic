72d6e52ab56de4ba78019710dec7efe9f5d62685
ZorikovPasha <zorikov.pasha@mail.ru>
2021-09-17 22:25:13 +0300

    commit just before wrating css when everything is set up

--- gulpfile.js
+++ gulpfile.js
@@ -14,12 +14,12 @@ var gulp = require('gulp'),
 gulp.task('cssLibsMinify', function() {
   return gulp.src([
     // 'node_modules/normalize.css/normalize.css',
-    // 'node_modules/slick-carousel/slick/slick.css',
+    'node_modules/slick-carousel/slick/slick.css',
     // 'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
     // 'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
     // 'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css',
     // 'node_modules/rateyo/lib/cjs/rateyo.css'
-    // 'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
   ])
     .pipe(concat('libs.min.css'))
     .pipe(cssmin())
@@ -30,13 +30,13 @@ gulp.task('cssLibsMinify', function() {
 
 gulp.task('jsLibsMinify', function() {
   return gulp.src([
-    // 'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/slick-carousel/slick/slick.js',
     // 'node_modules/ion-rangeslider/js/ion.rangeSlider.min.js',
     // 'node_modules/jquery-form-styler/dist/jquery.formstyler.min.js',
     // 'node_modules/rateyo/lib/cjs/rateyo.min.js',
     // 'node_modules/mixitup/dist/mixitup.min.js',
-    // 'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
-    // 'node_modules/inputmask/dist/jquery.inputmask.min.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    'node_modules/inputmask/dist/jquery.inputmask.min.js',
     // 'node_modules/dotdotdot-js/dist/dotdotdot.js',
     // 'node_modules/wowjs/dist/wow.min.js',
 
